c4921c85b281dc5dcf5fb7675f54d0ee367a8103
Сергей <kalininy21@gmail.com>
2023-01-25 21:24:38 +0300

    Добавьте файлы проекта.

---
+++ Entities/Configuration/UserConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Configuration
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasData
+            (
+            new User
+            {
+                Id = new Guid("8615e23f-2548-4ef7-a440-af6edc214fb0"),
+                Name = "Alpha",
+                Addres = "Moscow",
+                Phone = "89279988777"
+            },
+            new User
+            {
+                Id = new Guid("713a847a-2875-469d-aefb-fd7bb283a8d4"),
+                Name = "Delta",
+                Addres = "Saransk",
+                Phone = "89279966777"
+            });
+        }
+    }
+}
---
+++ Entities/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class User
+    {
+        [Column("UsersId")]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Company name is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for the Id is 60 characters.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Company address is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum length for rhe Name is 60 characte")]
+        public string Addres { get; set; }
+        public string Phone { get; set; }
+    }
+}
